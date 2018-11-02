3032dc66ce8a6070434a6e08b4e07501e7004284
DouwePieterReitsma <d.p.reitsma@gmail.com>
2018-11-02 14:40:30 +0100

    .gitignore aangepast

---
+++ besturingseenheden/src/main.c
@@ -0,0 +1,18 @@
+/*
+ * Besturingseenheden.c
+ *
+ * Created: 2-11-2018 14:27:32
+ * Author : dprei
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
