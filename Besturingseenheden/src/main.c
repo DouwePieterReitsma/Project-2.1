e0be9443ae31b10fa37e244a07993af665704df6
DouwePieterReitsma <d.p.reitsma@gmail.com>
2018-11-04 20:37:15 +0100

    Bestanden vergeten

--- Besturingseenheden/src/main.c
+++ Besturingseenheden/src/main.c
@@ -6,11 +6,19 @@
  */ 
 
 #include <avr/io.h>
+#define F_CPU 16E6
+#include <avr/delay.h>
+
+#include "common/serial.h"
 
 int main(void)
 {
-    /* Replace with your application code */
+    initialize_serial_ports();
+    
     while (1) 
     {
+        transmit_message("Hello World!");
+        
+        _delay_ms(1000);
     }
 }
