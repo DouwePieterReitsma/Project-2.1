d8c90330f062c074f3662871d1f5267cf5584187
DouwePieterReitsma <d.p.reitsma@gmail.com>
2018-11-15 12:33:25 +0100

    mapje aangemaakt

---
+++ Besturingseenheden/src/distance_sensor/distance_sensor.c
@@ -0,0 +1,8 @@
+/*
+ * distance_sensor.c
+ *
+ * Created: 15-11-2018 12:27:34
+ *  Author: dprei
+ */ 
+
+#include "distance_sensor.h"
---
+++ Besturingseenheden/src/distance_sensor/distance_sensor.h
@@ -0,0 +1,14 @@
+/*
+ * distance_sensor.h
+ *
+ * Created: 15-11-2018 12:27:22
+ *  Author: dprei
+ */ 
+
+
+#ifndef DISTANCE_SENSOR_H_
+#define DISTANCE_SENSOR_H_
+
+
+
+#endif /* DISTANCE_SENSOR_H_ */
