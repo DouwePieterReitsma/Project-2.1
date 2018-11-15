52f555f34564e55c6874879725918bd96eefd1a7
DouwePieterReitsma <d.p.reitsma@gmail.com>
2018-11-15 12:23:44 +0100

    asdfasdf

---
+++ Besturingseenheden/src/light_sensor/light_sensor.c
@@ -0,0 +1,6 @@
+/*
+ * light_sensor.c
+ *
+ * Created: 15-11-2018 12:21:24
+ *  Author: dprei
+ */
---
+++ Besturingseenheden/src/light_sensor/light_sensor.h
@@ -0,0 +1,16 @@
+/*
+ * light_sensor.h
+ *
+ * Created: 15-11-2018 12:20:56
+ *  Author: dprei
+ */ 
+
+
+#ifndef LIGHT_SENSOR_H_
+#define LIGHT_SENSOR_H_
+
+
+
+
+
+#endif /* LIGHT_SENSOR_H_ */
